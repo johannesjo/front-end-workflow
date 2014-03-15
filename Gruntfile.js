0f2c1ea653be679affca7aa313e3fbeea188043e
johannesjo <johannes.millan@gmail.com>
2014-03-15 14:21:22 +0100

    added grunt live-reload example

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dev: {
+                files: [{
+                    expand: true,
+                    cwd: 'styles',
+                    src: ['*.scss'],
+                    dest: 'styles',
+                    ext: '.css'
+                }]
+            }
+        },
+        watch: {
+            options: {
+                livereload: true,
+            },
+            css: {
+                files: ['styles/*.scss'],
+                tasks: ['sass']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.task.registerTask('default', ['watch']);
+};
